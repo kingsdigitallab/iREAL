8dc76940481b93d489d4e242c594d780d6053792
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-08-16 18:28:07 +0800

    feat(frontend): add component to render internal links

---
+++ frontend/src/lib/components/BaseLink.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { base } from '$app/paths';
+
+	export let href;
+
+	// ensure the href starts with a forward slash if it doesn't already
+	$: processedHref = href.startsWith('/') ? `${base}${href}` : `${base}/${href}`;
+</script>
+
+<a href={processedHref} {...$$restProps} on:click>
+	<slot />
+</a>
+
+<style>
+	.title {
+		font-size: var(--font-size-4);
+		font-weight: bold;
+		margin-block: 0;
+	}
+</style>
