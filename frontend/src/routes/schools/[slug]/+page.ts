e6e482963c4bade0a69d4f1f3e3fb4c43066ad88
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-08-28 21:43:48 +0800

    fix(frontend): remove getEntries function
    It is no longer needed for the build process.

--- frontend/src/routes/schools/[slug]/+page.ts
+++ frontend/src/routes/schools/[slug]/+page.ts
@@ -21,9 +21,3 @@ export const load = (async ({ params, parent }) => {
 		error(404, `Error loading school "${params.slug}": ${(e as Error).message}`);
 	}
 }) satisfies PageLoad;
-
-export const entries = (async () => {
-	const schools = await getSchools();
-
-	return schools.map((school) => ({ slug: school.slug }));
-}) satisfies EntryGenerator;
