327d5ff4cb5b891f838349969c0bfe7452b65e8d
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-08-29 18:43:02 +0800

    feat(frontend): add view to display organisations

---
+++ frontend/src/routes/organisations/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import FacetDistribution from '$lib/components/FacetDistribution.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<section>
+	<hgroup>
+		<h1>Distribution of the extracted keywords</h1>
+		<p>Explore a visualisation of the distribution of extracted keywords.</p>
+	</hgroup>
+	{#await data.schools}
+		<section aria-busy="true">Loading data...</section>
+	{:then schools}
+		<FacetDistribution {schools} label="organisation" field="organizations" />
+	{/await}
+</section>
