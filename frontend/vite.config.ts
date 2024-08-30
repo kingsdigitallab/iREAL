24d1b3f0f33184cb13597208ca7e9698c9fc376e
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-08-30 17:02:33 +0800

    feat(frontend): display the version in the dashboard

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import pkg from './package.json';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {
+		'import.meta.env.APP_VERSION': JSON.stringify(pkg.version)
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
