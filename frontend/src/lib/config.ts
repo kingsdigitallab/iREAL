dc6d8f0ada3a3d663b8b1efa5da58e42fd706a88
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-09-27 16:10:24 +0100

    feat(frontend): add rag frontend

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -3,7 +3,19 @@ import { dev } from '$app/environment';
 export const title = 'iREAL';
 export const description =
 	'Inclusive Requirements Elicitation for AI in Libraries to Support Respectful Management of Indigenous Knowledges';
-export const url = dev ? 'https://localhost:5173/' : 'https://kingsdigitallab.github.io/ireal/';
+export const url = dev ? 'http://localhost:5173/' : 'https://kingsdigitallab.github.io/ireal/';
+export const apiUrl = dev
+	? 'http://localhost:8000/api/query'
+	: 'https://ireal.kdl.kcl.ac.uk/api/query';
+
+export const promptTemplate = `
+
+Context information is below:
+---------------------
+{context_str}
+---------------------
+Given this information, please answer the question: {query_str}
+`;
 
 export const github = 'https://github.com/kingsdigitallab/iREAL';
 
