37431cf413e786d3ecc50cb60ef02d7fac4741c4
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-09-27 16:10:24 +0100

    feat(frontend): add rag frontend

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -34,6 +34,7 @@
 			<ul>
 				<li><BaseLink href="about" navItem={true}>About</BaseLink></li>
 				<li><BaseLink href="schools" navItem={true}>School records</BaseLink></li>
+				<li><BaseLink href="chat" navItem={true}>Chat</BaseLink></li>
 				<li><BaseLink href="keywords" navItem={true}>Keywords</BaseLink></li>
 				<li><BaseLink href="topics" navItem={true}>Topics</BaseLink></li>
 				<li><BaseLink href="organisations" navItem={true}>Organisations</BaseLink></li>
@@ -70,6 +71,7 @@
 
 	main {
 		margin-block: var(--pico-block-spacing-vertical);
+		height: 100%;
 	}
 
 	footer {
