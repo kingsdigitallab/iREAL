8834708cf4083392b9ed2f84f41b0255d5445b2d
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-10-03 10:27:12 +0100

    feat(frontend): add disclaimer and moved version and github links to the footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -23,11 +23,6 @@
 			<ul>
 				<li>
 					<BaseLink href="/"><strong>{config.title}</strong></BaseLink>
-					<code class="version">
-						<a href="https://github.com/kingsdigitallab/iReal/blob/v{version}/CHANGELOG.md"
-							>v{version}</a
-						>
-					</code>
 					{#if dev}<mark>dev</mark>{/if}
 				</li>
 			</ul>
@@ -40,16 +35,42 @@
 				<li><BaseLink href="organisations" navItem={true}>Organisations</BaseLink></li>
 				<li><BaseLink href="map" navItem={true}>Map</BaseLink></li>
 				<li class="separator">|</li>
-				<li>
-					<a href={config.github} aria-label="GitHub repository"><GithubIcon /></a>
-				</li>
 				<li><ThemeToggle /></li>
 			</ul>
 		</nav>
 	</header>
 	<main class="container"><slot /></main>
 	<footer class="container">
-		<BaseLink href="/">{config.title}</BaseLink>
+		<section class="disclaimer">
+			This is a prototype designed to stimulate discussion about the use of AI with Indigenous data.
+			It may contain errors, inconsistencies, or unexpected behaviors. While exploring, consider the
+			potential benefits and challenges of using AI to interact with Indigenous records. How might
+			it change our approach to research? What ethical considerations arise? Please use the
+			information provided with caution and verify important details. Your feedback and reflections
+			on this AI application are valuable for both improving the system and fostering a broader
+			dialogue on AI in Indigenous data contexts.
+		</section>
+		<nav>
+			<ul>
+				<li><BaseLink href="/">{config.title}</BaseLink></li>
+				<li class="separator">|</li>
+				<li>Designed and developed by <a href="https://kdl.kcl.ac.uk/">King's Digital Lab</a></li>
+			</ul>
+			<ul>
+				<li>
+					<code class="version">
+						<a href="https://github.com/kingsdigitallab/iReal/blob/v{version}/CHANGELOG.md">
+							v{version}
+						</a>
+					</code>
+				</li>
+				<li>
+					<a href={config.github} aria-label="GitHub repository">
+						<GithubIcon />
+					</a>
+				</li>
+			</ul>
+		</nav>
 	</footer>
 </div>
 
@@ -74,9 +95,16 @@
 		height: 100%;
 	}
 
-	footer {
+	footer,
+	footer nav {
 		border-top: 1px solid var(--pico-muted-border-color);
+		font-size: 0.8rem;
 		margin-block: var(--pico-block-spacing-vertical);
 		padding-block: var(--pico-block-spacing-vertical);
+	}
+
+	.disclaimer {
+		color: var(--pico-muted-color);
+		text-align: justify;
 	}
 </style>
